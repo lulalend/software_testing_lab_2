bb2e8303c9c7dd7c36432dfe4ad83ed42e8d1e20
iwishyoujoy <dasha@skv.name>
2024-04-04 21:30:59 +0300

    tests for sin func

---
+++ src/test/java/ru/itmo/BasicTest.java
@@ -0,0 +1,6 @@
+package ru.itmo;
+
+public class BasicTest {
+    protected final static double PRECISION = 0.00001;
+    protected final static double PRECISION_ASSERT = 0.00001;
+}
---
+++ src/test/java/ru/itmo/SinTest.java
@@ -0,0 +1,84 @@
+package ru.itmo;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import ru.itmo.funcs.basic.trig.SinFunc;
+
+public class SinTest extends BasicTest {
+    
+    private SinFunc sin;
+
+    @BeforeEach
+    public void setUp() {
+        sin = new SinFunc();
+    }
+
+    @ParameterizedTest
+    @MethodSource("zeroValues")
+    public void testZeroValues(double x) {
+        Assertions.assertEquals(0, sin.calculate(x, PRECISION), PRECISION);
+    }
+
+    @ParameterizedTest
+    @MethodSource("oneValues")
+    public void testOneValues(double x) {
+        Assertions.assertEquals(1, sin.calculate(x, PRECISION), PRECISION);
+    }
+
+    @ParameterizedTest
+    @MethodSource("minusOneValues")
+    public void testMinusOneValues(double x) {
+        Assertions.assertEquals(-1, sin.calculate(x, PRECISION), PRECISION);
+    }
+
+    @ParameterizedTest
+    @MethodSource("middleValues")
+    public void testMiddleValues(double x) {
+        Assertions.assertEquals(Math.sin(Math.PI / 4), sin.calculate(x, PRECISION), PRECISION);
+    }
+
+    private static Stream<Arguments> zeroValues() {
+        return Stream.of(
+                Arguments.of(0),
+                Arguments.of(Math.PI),
+                Arguments.of(Math.PI * 2));
+    }
+
+    private static Stream<Arguments> oneValues() {
+        return Stream.of(
+                Arguments.of(- 11 * Math.PI / 2),
+                Arguments.of(- 7 * Math.PI / 2),
+                Arguments.of(- 3 * Math.PI / 2),
+                Arguments.of(Math.PI / 2),
+                Arguments.of(5 * Math.PI / 2),
+                Arguments.of(9 * Math.PI / 2));
+    }
+
+    private static Stream<Arguments> minusOneValues() {
+        return Stream.of(
+                Arguments.of(- 9 * Math.PI / 2),
+                Arguments.of(- 5 * Math.PI / 2),
+                Arguments.of(- Math.PI / 2),
+                Arguments.of(3 * Math.PI / 2),
+                Arguments.of(7 * Math.PI / 2),
+                Arguments.of(11 * Math.PI / 2));
+    }
+
+    private static Stream<Arguments> middleValues() {
+        return Stream.of(
+                Arguments.of(- 7 * Math.PI / 4),
+                Arguments.of(- 5 * Math.PI / 4),
+                Arguments.of(Math.PI / 4),
+                Arguments.of(3 * Math.PI / 4),
+                Arguments.of(9 * Math.PI / 4),
+                Arguments.of(11 * Math.PI / 4));
+    }
+
+
+}
