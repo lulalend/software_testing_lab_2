84182487e88113979bd282dec2dcde5f3852caa7
iwishyoujoy <dasha@skv.name>
2024-04-04 21:02:36 +0300

    fixed cos func

--- src/main/java/ru/itmo/funcs/basic/trig/CosFunc.java
+++ src/main/java/ru/itmo/funcs/basic/trig/CosFunc.java
@@ -8,6 +8,26 @@ public class CosFunc {
     }
 
     public double calculate(double x, double eps) {
-        return 1/ sin.calculate(x, eps);
+        if (isCosPositive(x)) {
+            return Math.sqrt(1 - Math.pow(sin.calculate(x, eps), 2));
+        }
+        return - Math.sqrt(1 - Math.pow(sin.calculate(x, eps), 2));
+    }
+
+    public boolean isCosPositive(double x) {
+        // нормализуем угол в диапазон от 0 до 2π
+        double normalizedX = x % (2 * Math.PI);
+        if (normalizedX < 0) {
+            normalizedX += 2 * Math.PI;
+        }
+    
+        // определяем четверть
+        if (normalizedX >= 0 && normalizedX <= Math.PI / 2 ||
+            normalizedX >= 3 * Math.PI / 2 && normalizedX <= 2 * Math.PI) {
+            // 1 и 4 четверти
+            return true;
+        }
+        // 2 и 3 четверти
+        return false;
     }
 }
